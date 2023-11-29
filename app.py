dac82224b38e27f0f57eebf9c5b9ec757bb74f37
Ryu Tanabe <bellx2@gmail.com>
2023-11-29 21:50:08 +0900

    説明を一部修正

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 st.title("More 500KB")
 
 st.write("謎制限で500KB以上の画像ファイルが必要なときに使うツールです。圧縮率を下げて画像を少しずつ拡大することで、500KB以上のJEPG画像を生成します。プレビューは拡張されたサイズのPNG画像を表示します。")
-st.write("一時領域で処理するため管理者は画像を見ることはできません。")
+st.write("一時領域で処理するため管理者も画像を見ることはできません。")
 
 image_file = st.file_uploader("画像をアップロード", type=['png', 'jpeg', 'jpg'])
 if image_file is not None:
@@ -28,4 +28,4 @@ if image_file is not None:
                 os.path.splitext(image_file.name)[0] + "_500k.jpg"),
             mime="image/jpeg"
         )
-        st.image(img, caption="PNG画像", output_format='png')
+        st.image(img, caption="PNG画像 長押しで保存", output_format='png')
