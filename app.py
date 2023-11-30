79ba4d9ae6c99efa5d22619ec4c04798c991153a
Ryu Tanabe <bellx2@gmail.com>
2023-11-30 11:32:36 +0900

    アイコンを追加して可愛くした

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ import tempfile
 import urllib.parse
 import os
 
-st.title("Over 500KB")
+st.title(":frame_with_picture: Over 500KB")
 
-st.write("元画像の圧縮比率とサイズを変えて500KB以上の画像ファイルを生成します。画質は向上しません。")
-st.write("アップロードデータは画像処理のために使用し、保存していません。")
+st.write("元画像の圧縮比率とサイズを変えて500KB以上の画像ファイルを生成します。画質は向上しません。アップロードデータは保存していません。")
 
-image_file = st.file_uploader("画像をアップロードしてください", type=['png', 'jpeg', 'jpg'])
+image_file = st.file_uploader("画像をアップロードしてください", type=['png', 'jpeg', 'jpg'], accept_multiple_files=False)
 if image_file is not None:
     file_details = {"FileName": image_file.name, "FileType": image_file.type}
     img = Image.open(image_file).convert('RGB')
@@ -17,15 +16,17 @@ if image_file is not None:
         for i in range(10):
             img = img.resize(
                 (int(img.size[0]*(1+i*0.2)), int(img.size[1]*(1+i*0.2))))
-            img.save(tmp.name, quality=100)
-            file_size = os.path.getsize(tmp.name)/1024
+            img.save(tmp.name, quality=100) # 100 disables portions of the JPEG compression algorithm
+            file_size = os.path.getsize(tmp.name)/1024  # KB
             if file_size > 500:
                 break
+        st.header("生成完了！ :grin:")
         st.download_button(
-            label="JPEG画像ダウンロード " + str(int(file_size)) + "KB",
+            label="JPEG形式 : クリックでダウンロード " + str(int(file_size)) + "KB",
             data=open(tmp.name, 'rb').read(),
             file_name=urllib.parse.quote(
                 os.path.splitext(image_file.name)[0] + "_500k.jpg"),
             mime="image/jpeg"
         )
-        st.image(img, caption="PNG画像 長押しで保存", output_format='png')
+        st.write("PNG形式 : 画像長押しで保存")
+        st.image(img, output_format='png')
